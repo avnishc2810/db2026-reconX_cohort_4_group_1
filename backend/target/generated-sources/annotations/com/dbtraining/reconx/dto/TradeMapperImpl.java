47247f775de3d48968af52c94b8e46e003512cf7
aprvx <apurvaygokhale@gmail.com>
2026-07-29 11:14:09 +0530

    TICKET-ADV026

--- backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
+++ backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-28T23:29:40+0530",
-    comments = "version: 1.6.2, compiler: javac, environment: Java 23.0.1 (Oracle Corporation)"
+    date = "2026-07-29T10:54:00+0530",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.9 (Ubuntu)"
 )
 @Component
 public class TradeMapperImpl implements TradeMapper {
