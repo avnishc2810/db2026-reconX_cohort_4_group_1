6cc6ae1df8e943788333af4fff02988a963758ba
aprvx <apurvaygokhale@gmail.com>
2026-07-29 11:56:38 +0530

    TICKET-ADV027

--- backend/src/main/java/com/dbtraining/reconx/model/TradeType.java
+++ backend/src/main/java/com/dbtraining/reconx/model/TradeType.java
@@ -1,8 +1,10 @@
 package com.dbtraining.reconx.model;
 
+import java.util.Comparator;
 import java.time.LocalDate;
 
 public sealed interface TradeType
+        extends Comparable<TradeType>
         permits EquityTrade, FXTrade, BondTrade, DerivativeTrade {
 
     TradeRef tradeRef();
@@ -12,6 +14,14 @@ public sealed interface TradeType
     LocalDate tradeDate();
 
     AssetClass assetClass();
+        Comparator<TradeType> NATURAL = Comparator
+            .comparing(TradeType::tradeDate).reversed()
+            .thenComparing(t -> t.tradeRef().value());
+
+    @Override
+    default int compareTo(TradeType other) {
+        return NATURAL.compare(this, other);
+    }
 
     enum AssetClass {
         EQUITY,
